b160f637c9f6e1bc1199a5cec4b41278381bb7a4
Han <tranthithuhan@gmail.com>
2018-05-14 19:45:02 +0200

    Add  board

---
+++ src/containers/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div className="board-item">
+                coiucou
+            </div>
+        );
+    }
+};
+
+export default Board;
